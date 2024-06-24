b20c96310559e4f147bdc97d604484bb05d9117b
Jonathan Breidfjord <jbreidfjord@gmail.com>
2024-06-23 20:04:26 -0600

    total internal reflection

--- src/material.rs
+++ src/material.rs
@@ -3,7 +3,7 @@ use crate::ray::Ray;
 use crate::vec3::{Color, Vec3};
 
 pub trait Material {
-    fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(Color, Ray)>;
+    fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(&Color, Ray)>;
 }
 
 pub struct Lambertian {
@@ -17,7 +17,7 @@ impl Lambertian {
 }
 
 impl Material for Lambertian {
-    fn scatter(&self, _ray: &Ray, hit_record: &HitRecord) -> Option<(Color, Ray)> {
+    fn scatter(&self, _ray: &Ray, hit_record: &HitRecord) -> Option<(&Color, Ray)> {
         let mut scatter_direction = hit_record.normal() + Vec3::random_unit_vector();
 
         // Catch degenerate scatter direction
@@ -26,7 +26,7 @@ impl Material for Lambertian {
         }
 
         let scattered = Ray::new(hit_record.p().clone(), scatter_direction);
-        let attenuation = self.albedo.clone();
+        let attenuation = &self.albedo;
 
         Some((attenuation, scattered))
     }
@@ -47,11 +47,11 @@ impl Metal {
 }
 
 impl Material for Metal {
-    fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(Color, Ray)> {
+    fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(&Color, Ray)> {
         let reflected = ray.direction().reflect(hit_record.normal());
         let reflected = reflected.unit() + (self.fuzz * Vec3::random_unit_vector());
         let scattered = Ray::new(hit_record.p().clone(), reflected);
-        let attenuation = self.albedo.clone();
+        let attenuation = &self.albedo;
         if scattered.direction().dot(hit_record.normal()) > 0. {
             Some((attenuation, scattered))
         } else {
@@ -64,17 +64,20 @@ pub struct Dielectric {
     /// Refractive index in vacuum or air, or the ratio of the material's refractive index over
     /// the refractive index of the enclosing media
     refraction_index: f64,
+    color: Color,
 }
 
 impl Dielectric {
     pub const fn new(refraction_index: f64) -> Self {
-        Self { refraction_index }
+        Self {
+            refraction_index,
+            color: Color::new(1., 1., 1.),
+        }
     }
 }
 
 impl Material for Dielectric {
-    fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(Color, Ray)> {
-        let attenuation = Color::new(1., 1., 1.);
+    fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(&Color, Ray)> {
         let refraction_ratio = if hit_record.front_face() {
             1. / self.refraction_index
         } else {
@@ -82,9 +85,18 @@ impl Material for Dielectric {
         };
 
         let unit_direction = ray.direction().unit();
-        let refracted = unit_direction.refract(hit_record.normal(), refraction_ratio);
 
-        let scattered = Ray::new(hit_record.p().clone(), refracted);
-        Some((attenuation, scattered))
+        let cos_theta = (-&unit_direction).dot(hit_record.normal()).min(1.0);
+        let sin_theta = cos_theta.mul_add(-cos_theta, 1.0).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract {
+            unit_direction.reflect(hit_record.normal())
+        } else {
+            unit_direction.refract(hit_record.normal(), refraction_ratio)
+        };
+
+        let scattered = Ray::new(hit_record.p().clone(), direction);
+        Some((&self.color, scattered))
     }
 }
