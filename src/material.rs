f82e505ec207484ba64812a1ed2799d2a103acaf
Jonathan Breidfjord <jbreidfjord@gmail.com>
2024-06-23 21:18:03 -0600

    schlick approximation

--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,5 @@
+use rand::{Rng, thread_rng};
+
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
 use crate::vec3::{Color, Vec3};
@@ -74,6 +76,13 @@ impl Dielectric {
             color: Color::new(1., 1., 1.),
         }
     }
+
+    /// Schlick's approximation for reflectance
+    fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
+        let r0 = (1. - refraction_index) / (1. + refraction_index);
+        let r0 = r0.powi(2);
+        (1. - r0).mul_add((1. - cosine).powi(5), r0)
+    }
 }
 
 impl Material for Dielectric {
@@ -89,12 +98,14 @@ impl Material for Dielectric {
         let cos_theta = (-&unit_direction).dot(hit_record.normal()).min(1.0);
         let sin_theta = cos_theta.mul_add(-cos_theta, 1.0).sqrt();
 
+        let mut rng = thread_rng();
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
-            unit_direction.reflect(hit_record.normal())
-        } else {
-            unit_direction.refract(hit_record.normal(), refraction_ratio)
-        };
+        let direction =
+            if cannot_refract || Self::reflectance(cos_theta, refraction_ratio) > rng.gen() {
+                unit_direction.reflect(hit_record.normal())
+            } else {
+                unit_direction.refract(hit_record.normal(), refraction_ratio)
+            };
 
         let scattered = Ray::new(hit_record.p().clone(), direction);
         Some((&self.color, scattered))
